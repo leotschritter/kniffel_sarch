c6908f07d768b93daf7658e4c1a76ad46ff44f3c
benediktscheffel <benedikt.scheffel@hotmail.de>
2024-04-23 12:06:24 +0200

    SARCH-05: FileIO Tests fixed

--- fileIO/src/test/scala/de/htwg/sa/kniffel/fileio/fileIOXmlImpl/FileIOXmlSpec.scala
+++ fileIO/src/test/scala/de/htwg/sa/kniffel/fileio/fileIOXmlImpl/FileIOXmlSpec.scala
@@ -1,16 +1,22 @@
 package de.htwg.sa.kniffel.fileio.fileIOXmlImpl
 
-import de.htwg.sa.kniffel.field.fieldBaseImpl.Field
-import de.htwg.sa.kniffel.dicecup.dicecupBaseImpl.DiceCup
-import de.htwg.sa.kniffel.game.gameBaseImpl.{Game, Player}
+
 import org.scalatest.matchers.should.Matchers.*
 import org.scalatest.wordspec.AnyWordSpec
 
 class FileIOXmlSpec extends AnyWordSpec {
   "A Game" when {
     val fileIO = new FileIO()
-    val players: List[Player] = List(Player(0, "Player1"), Player(1, "Player2"), Player(2, "Player3"), Player(3, "Player4"))
-    val game = Game(players, players.head, players.length * 13, List.fill(players.length, 6)(0))
+    val game = "{\"game\":{\"nestedList\":\"0,0,0,0,0,0;0,0,0,0,0,0\",\"remainingMoves\":26,\"currentPlayerID\":0," +
+      "\"currentPlayerName\":\"Player 1\",\"players\":[{\"id\":0,\"name\":\"Player 1\"},{\"id\":1," +
+      "\"name\":\"Player 2\"}]}}"
+    val field = "{\"field\":{\"numberOfPlayers\":2,\"rows\":[[null,null],[null,null],[null,null],[null,null]," +
+      "[null,null],[null,null],[null,null],[null,null],[null,null],[null,null],[null,null],[null,null],[null,null]," +
+      "[null,null],[null,null],[null,null],[null,null],[null,null],[null,null]]}}"
+
+    val diceCup = "{\"dicecup\":{\"stored\":[2,2],\"incup\":[2,2,2],\"remainingDices\":2}}"
+    
+
     "saved to xml" should {
       "equal the previously saved game after loading" in {
         fileIO.saveGame(game)
@@ -18,17 +24,15 @@ class FileIOXmlSpec extends AnyWordSpec {
       }
     }
     "A Field" when {
-      val field: Field = new Field(2)
       "saved to xml" should {
         "equal the previously saved field after loading" in {
 
-          fileIO.saveField(field, field.matrix)
+          fileIO.saveField(field)
           fileIO.loadField should be(field)
         }
       }
     }
     "A DiceCup" when {
-      val diceCup: DiceCup = DiceCup(List(1, 1, 1, 1), List(1), 1)
       "saved to xml" should {
         "equal the previously saved DiceCup after loading" in {
           fileIO.saveDiceCup(diceCup)
