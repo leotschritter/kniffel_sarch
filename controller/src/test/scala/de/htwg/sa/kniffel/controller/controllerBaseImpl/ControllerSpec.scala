8aa17dd17c652a35bac7f17ff749edac664287f0
Leo Tschritter <leotschritter@gmail.com>
2024-04-23 14:28:26 +0200

    SARCH-05: Fixed Controller Test an modified pipeline

--- controller/src/test/scala/de/htwg/sa/kniffel/controller/controllerBaseImpl/ControllerSpec.scala
+++ controller/src/test/scala/de/htwg/sa/kniffel/controller/controllerBaseImpl/ControllerSpec.scala
@@ -1,16 +1,16 @@
 package de.htwg.sa.kniffel.controller.controllerBaseImpl
 
-import de.htwg.sa.kniffel.dicecup.IDiceCup
-import de.htwg.sa.kniffel.dicecup.dicecupBaseImpl.DiceCup
-import de.htwg.sa.kniffel.game.gameBaseImpl.{Game, Player}
-import de.htwg.sa.kniffel.util.{Event, Move, Observer}
+import de.htwg.sa.kniffel.util.HttpUtil.sendRequest
+import de.htwg.sa.kniffel.util.{Event, HttpUtil, Move, Observer}
 import org.scalatest.matchers.should.Matchers.*
 import org.scalatest.wordspec.AnyWordSpec
+import play.api.libs.json.{JsDefined, JsNull, JsNumber, Json}
+
 
 class ControllerSpec extends AnyWordSpec {
   "The Controller" should {
-    val controller = Controller()
-    val controller2 = Controller()
+    val controller = new Controller()
+    val controller2 = new Controller()
 
     "notify its observers on change" in {
       class TestObserver(controller: Controller) extends Observer :
@@ -28,23 +28,32 @@ class ControllerSpec extends AnyWordSpec {
     }
 
     "dices are put out the Dice Cup or in" should {
-      val sortOut: IDiceCup = controller.putOut(controller.diceCup.inCup.take(2))
+      controller.diceCup = sendRequest("diceCup/dice", controller.diceCup)
+      val inCup: List[Int] = (Json.parse(controller.diceCup) \ "dicecup" \ "incup").as[List[Int]]
+      controller.diceCup = controller.putOut(inCup.take(2))
+      val sortOut = Json.parse(controller.diceCup) \ "dicecup"
       "be inserted into the locked list of a new DiceCup Object" in {
-        sortOut.locked.size should be(2)
-        sortOut.inCup.size should be(3)
+        (sortOut \ "stored").as[List[Int]].size should be (2)
+        (sortOut \ "incup").as[List[Int]].size should be (3)
       }
       "be inserted into the inCup list of a new DiceCup Object" in {
-        val putIn: IDiceCup = controller.putIn(controller.diceCup.locked.take(2))
-        putIn.locked.size should be(0)
-        putIn.inCup.size should be(5)
+        val locked: List[Int] = (Json.parse(controller.diceCup) \ "dicecup" \ "stored").as[List[Int]]
+        val putIn = Json.parse(controller.putIn(locked)) \ "dicecup"
+        (putIn \ "stored").as[List[Int]].size should be (0)
+        (putIn \ "incup").as[List[Int]].size should be (5)
       }
     }
 
     "dices are thrown" should {
       "contain two lists with all dices" in {
-        val thrownDiceCup: IDiceCup = controller.dice()
-        thrownDiceCup.inCup.size + thrownDiceCup.locked.size should be(5)
-        thrownDiceCup.inCup.foreach {
+        val thrownDiceCup = Json.parse(controller.dice()) \ "dicecup"
+        (thrownDiceCup \ "incup").as[List[Int]].size + (thrownDiceCup \ "stored").as[List[Int]].size should be(5)
+        (thrownDiceCup \ "incup").as[List[Int]].foreach {
+          s =>
+            s should be < 7
+            s should be > 0
+        }
+        (thrownDiceCup \ "stored").as[List[Int]].foreach {
           s =>
             s should be < 7
             s should be > 0
@@ -55,51 +64,98 @@ class ControllerSpec extends AnyWordSpec {
     "set a new Game object" when {
       "finishing a move" in {
         controller2.next()
-        controller2.game should be (Game(List(Player(0,"Player 1"), Player(1,"Player 2")),Player(1,"Player 2"),25,List(List(0, 0, 0, 0, 0, 0), List(0, 0, 0, 0, 0, 0))))
+        controller2.game should be ("{\"game\":{\"nestedList\":\"0,0,0,0,0,0;0,0,0,0,0,0\",\"remainingMoves\":25,\"currentPlayerID\":1,\"currentPlayerName\":\"Player 2\",\"players\":[{\"id\":0,\"name\":\"Player 1\"},{\"id\":1,\"name\":\"Player 2\"}]}}")
       }
     }
     "after a Move" when {
       "write down the result" in {
-        val con = controller.nextRound()
-        con.remainingDices should be(2)
-        con.inCup.size should be(0)
-        con.locked.size should be(0)
+        val con = Json.parse(controller.nextRound()) \ "dicecup"
+        (con \ "remainingDices").as[Int] should be(2)
+        (con \ "incup").as[List[Int]].size should be(0)
+        (con \ "stored").as[List[Int]].size should be(0)
       }
     }
     "when toString is called" should {
       "toString" in {
-        controller.toString should be(controller.field.toString)
+        val controller3 = new Controller()
+        controller3.toString should be(
+          "    |P1 |P2 \n" +
+          "+---+---+---+\n" +
+          "|1  |   |   |\n" +
+          "+---+---+---+\n" +
+          "|2  |   |   |\n" +
+          "+---+---+---+\n" +
+          "|3  |   |   |\n" +
+          "+---+---+---+\n" +
+          "|4  |   |   |\n" +
+          "+---+---+---+\n" +
+          "|5  |   |   |\n" +
+          "+---+---+---+\n" +
+          "|6  |   |   |\n" +
+          "+---+---+---+\n" +
+          "|G  |   |   |\n" +
+          "+---+---+---+\n" +
+          "|B  |   |   |\n" +
+          "+---+---+---+\n" +
+          "|O  |   |   |\n" +
+          "+---+---+---+\n" +
+          "|3x |   |   |\n" +
+          "+---+---+---+\n" +
+          "|4x |   |   |\n" +
+          "+---+---+---+\n" +
+          "|FH |   |   |\n" +
+          "+---+---+---+\n" +
+          "|KS |   |   |\n" +
+          "+---+---+---+\n" +
+          "|GS |   |   |\n" +
+          "+---+---+---+\n" +
+          "|KN |   |   |\n" +
+          "+---+---+---+\n" +
+          "|CH |   |   |\n" +
+          "+---+---+---+\n" +
+          "|U  |   |   |\n" +
+          "+---+---+---+\n" +
+          "|O  |   |   |\n" +
+          "+---+---+---+\n" +
+          "|E  |   |   |\n" +
+          "+---+---+---+\n" +
+          "Im Becher: \n"+
+          "Rausgenommen: \n"+
+          "Verbleibende Würfe: 3\n"+
+          "Bitte wählen Sie aus: 1 2 3 4 5 6 3X 4X FH KS GS KN CH\n"+
+          "Player 1 ist an der Reihe.")
       }
     }
     "when undo/redo/put/save/load is called" should {
       "put" in {
         controller.put(Move(11, 0, 0))
-        controller.field.matrix.cell(0, 0).get should be(11)
+        (Json.parse(sendRequest("field/cell/0/0", controller.field)) \ "value").as[Int] should be (11)
       }
       "undo" in {
         controller.undo()
-        controller.field.matrix.cell(0, 0) should be(None)
+        (Json.parse(sendRequest("field/cell/0/0", controller.field)) \ "value") should be (JsDefined(JsNull))
       }
       "redo" in {
         controller.redo()
-        controller.field.matrix.cell(0, 0).get should be(11)
+        (Json.parse(sendRequest("field/cell/0/0", controller.field)) \ "value") should be (JsDefined(JsNumber(11)))
       }
       "save and loaded" in {
-        controller.diceCup = DiceCup(List(), List(1, 2, 3, 4, 5), 2)
+        controller.diceCup = "{\"dicecup\":{\"stored\":[],\"incup\":[1,2,3,4,5],\"remainingDices\":1}}"
         controller.save()
         controller.load()
-        controller.diceCup should be (DiceCup(List(), List(1, 2, 3, 4, 5), 2))
+        controller.diceCup should be ("{\"dicecup\":{\"stored\":[],\"incup\":[1,2,3,4,5],\"remainingDices\":1}}")
       }
     }
+
     "when game quit" should {
       "end the game" in {
-        val controller3: Controller = controller
-        controller.quit().toString should be(controller3.quit().toString)
+        val controller4: Controller = controller
+        controller.quit() should be(controller4.quit())
       }
     }
     "when no remaining moves are left game.next" should {
       "be None" in {
-        val game: Game = Game(List(Player(1, "Harald"), Player(2, "Dieter")), Player(1, "Harald"), 0, List())
+        val game = "{\"game\":{\"nestedList\":\"0,0,0,0,0,0;0,0,0,0,0,0\",\"remainingMoves\":0,\"currentPlayerID\":0,\"currentPlayerName\":\"Player 1\",\"players\":[{\"id\":0,\"name\":\"Player 1\"},{\"id\":1,\"name\":\"Player 2\"}]}}"
         controller.game = game
         controller.next()
         controller.game should be(game)
