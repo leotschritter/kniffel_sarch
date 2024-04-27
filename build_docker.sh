0d853cb806b628fff9173cd907eac89c4f9372ee
benediktscheffel <benedikt.scheffel@hotmail.de>
2024-04-27 13:08:09 +0200

    SARCH-06: Add shell-script to build docker images

---
+++ build_docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# List of module directories containing Dockerfiles
+MODULES=("dicecup" "field" "fileio" "game" "tui" "restcontroller") #"gui")
+
+# Function to build Dockerfile in a directory
+build_dockerfile() {
+    local module=$1
+    echo "Building Dockerfile in $module..."
+    docker build -t "$module"-image:latest $module
+}
+
+# Loop through each module and build Dockerfile
+for module in "${MODULES[@]}"; do
+    build_dockerfile "$module"
+done
+
+echo "All Dockerfiles built successfully!"
