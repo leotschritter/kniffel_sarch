ed3fa925bf12051e9da1e4f2157b8b58b9452b26
Leo Tschritter <leotschritter@gmail.com>
2024-06-10 16:11:51 +0200

    SARCH-10: Evaluator pattern now using ReactiveStreams to handle evaluations

--- dicecup/src/main/scala/de/htwg/sa/kniffel/dicecup/model/dicecupBaseImpl/DiceCup.scala
+++ dicecup/src/main/scala/de/htwg/sa/kniffel/dicecup/model/dicecupBaseImpl/DiceCup.scala
@@ -1,13 +1,24 @@
 package de.htwg.sa.kniffel.dicecup.model.dicecupBaseImpl
 
-import de.htwg.sa.kniffel.dicecup.model.{EvaluateStrategy, Evaluator, IDiceCup}
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.GraphDSL.Builder
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, Sink, Source}
+import akka.stream.{FlowShape, Graph}
+import de.htwg.sa.kniffel.dicecup.model.EvaluateStrategy.*
+import de.htwg.sa.kniffel.dicecup.model.{EvaluateStrategy, IDiceCup}
 import play.api.libs.json.{JsNumber, JsObject, JsValue, Json}
 
 import scala.collection.immutable.ListMap
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext}
 import scala.util.Random
 
 case class DiceCup(locked: List[Int], inCup: List[Int], remDices: Int) extends IDiceCup:
   def this() = this(List.fill(0)(0), List.fill(5)(Random.between(1, 7)), 2)
+
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val executionContext: ExecutionContext = system.dispatcher
 
   def dice(): Option[DiceCup] =
     if (remDices >= 0)
@@ -45,21 +56,39 @@ case class DiceCup(locked: List[Int], inCup: List[Int], remDices: Int) extends I
 
   private def mergeLists(list1: List[Int])(list2: List[Int]): List[Int] = list1 ::: list2
 
+  def evaluate(index: Int): Graph[FlowShape[List[Int], Int], NotUsed] = GraphDSL.create() { implicit builder: Builder[NotUsed] =>
+    import GraphDSL.Implicits.*
+    
+    val broadcast = builder.add(Broadcast[List[Int]](1))
+    val merge = builder.add(Merge[Int](1))
+    
+    val selectFlowShape = builder.add(
+      index match {
+        case 0 | 1 | 2 | 3 | 4 | 5 => sumFlow
+        case 9 => threeOfAKindFlow
+        case 10 => fourOfAKindFlow
+        case 11 => fullHouseFlow
+        case 12 => smallStreetFlow
+        case 13 => bigStreetFlow
+        case 14 => kniffelFlow
+        case 15 => sumFlow
+        case _ => zeroFlow
+      }
+    )
+    broadcast.out(0) ~> selectFlowShape ~> merge.in(0)
+    FlowShape(broadcast.in, merge.out)
+  }
+
   def result(index: Int): Int =
     val list: List[Int] = mergeLists(inCup)(locked)
     if (list.isEmpty)
       return 0
-    index match {
-      case 0 | 1 | 2 | 3 | 4 | 5 => list.filter(_ == index + 1).sum
-      case 9 => new Evaluator(EvaluateStrategy.threeOfAKind).result(list)
-      case 10 => new Evaluator(EvaluateStrategy.fourOfAKind).result(list)
-      case 11 => new Evaluator(EvaluateStrategy.fullHouse).result(list)
-      case 12 => new Evaluator(EvaluateStrategy.smallStreet).result(list)
-      case 13 => new Evaluator(EvaluateStrategy.bigStreet).result(list)
-      case 14 => new Evaluator(EvaluateStrategy.kniffel).result(list)
-      case 15 => new Evaluator(EvaluateStrategy.sumChance).result(list)
-      case _ => 0
-    }
+
+    val resultGraph = Source.single(if index < 6 && index >= 0 then list.filter(_ == index + 1) else list)
+      .via(Flow.fromGraph(evaluate(index)))
+      .runWith(Sink.head)
+      .map(result => result)
+    Await.result(resultGraph, Duration.Inf)
 
   def indexOfField: ListMap[String, Int] =
     ListMap("1" -> 0, "2" -> 1, "3" -> 2, "4" -> 3, "5" -> 4, "6" -> 5,
